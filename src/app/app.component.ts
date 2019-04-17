08ff5910b4ce64dd8d38a9df90dbb489f030a80c
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2019-04-17 13:32:04 -0500

    Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular8ivycompiler';
+  counter: number = 0;
+
+  onClick(){
+    this.counter++;
+  }
 }
